b2c4311b2cc064035ceed7dde554d1fb5f63863d
mateorinland <121818729+mateorinland@users.noreply.github.com>
2023-01-09 19:28:00 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+function getQuotes() {
+  return $.ajax({
+    headers: {
+      Accept: 'application/json'
+    },
+    url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+    success: function (jsonQuotes) {
+      if (typeof jsonQuotes === 'string') {
+        quotesData = JSON.parse(jsonQuotes);
+        console.log('quotesData');
+        console.log(quotesData);
+      }
+    }
+  });
+}
+
+function getRandomQuote() {
+  return quotesData.quotes[
+    Math.floor(Math.random() * quotesData.quotes.length)
+  ];
+}
+
+function getQuote() {
+  let randomQuote = getRandomQuote();
+  currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
+
+  $('#tweet-quote').attr(
+    'href',
+    'https://twitter.com/intent/tweet?hashtags=quotes&text=' +
+    encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
+
+ $('#text').text('"'+randomQuote.quote+'"');
+ $('#author').html("—"+randomQuote.author);
+
+}
+
+
+$(document).ready(function () {
+  getQuotes().then(() => {
+    getQuote();
+  });
+
+  
+  $('#new-quote').on('click', getQuote);
+});
